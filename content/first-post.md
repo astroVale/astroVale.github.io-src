427c767d6845d8e7912da055d0b9bbad4f320fd1
Valentina Scipione <vscipione.mail@gmail.com>
2017-03-08 17:19:41 +0100

    First commit.

---
+++ content/first-post.md
@@ -0,0 +1,10 @@
+Title: My first post
+Date: 2010-03-08 11:00
+Category: Python
+Tags: pelican, publishing
+Slug: my-super-post
+Authors: Valentina Scipione
+Summary: Short version for index and feeds
+
+This is the content of my super blog post.
+Happy Women's Day!!!
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Valentina Scipione'
+SITENAME = u"Data Science's Tales"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('LinkedIn', 'https://www.linkedin.com/in/atariah'),
+    #('You can modify those links in your config file', '#'),
+    )
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
