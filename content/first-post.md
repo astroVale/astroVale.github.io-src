7f463946222edaf01b2bf5264457c67f540dd3fb
Valentina Scipione <vscipione.mail@gmail.com>
2017-03-09 23:36:31 +0100

    changed theme to blueidea

--- content/first-post.md
+++ content/first-post.md
@@ -1,20 +1,6 @@
 Title: My first post
-Date: 2010-03-08 11:00
+Date: 2017-03-08 11:00
 Status: published
 
 This is the content of my first Pelican blog post.
 Happy Women's Day!!!
-
-
-<section>
-    <p id="post-share-links">
-        Do you like this article? Share on:
-        <a href="{{article.share_post['twitter']}}" target="_blank" title="Share on Twitter">Twitter</a>
-        ❄
-        <a href="{{article.share_post['facebook']}}" target="_blank" title="Share on Facebook">Facebook</a>
-        ❄
-        <a href="{{article.share_post['google-plus']}}" target="_blank" title="Share on Google Plus">Google+</a>
-        ❄
-        <a href="{{article.share_post['email']}}" target="_blank" title="Share via Email">Email</a>
-    </p>
-</section>
--- pelicanconf.py
+++ pelicanconf.py
@@ -20,29 +20,57 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (
-    ('LinkedIn', 'https://www.linkedin.com/in/valentinascipione'),
-    )
+# LINKS = (
+#     ('LinkedIn', 'https://www.linkedin.com/in/valentinascipione'),
+#     )
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/v_scipione'),
+SOCIAL = (('linkedin','https://www.linkedin.com/in/valentinascipione'),
+          ('twitter', 'https://twitter.com/v_scipione'),
           ('github', 'https://github.com/astroVale'),
-          ('linkedin','https://www.linkedin.com/in/valentinascipione'),
           ('stackoverflow','http://stackoverflow.com/users/7193879/valentina-scipione'),
-          ('envelope','mailto:vscipione.mail@gmail.com'))
+          ('mail','mailto:vscipione.mail@gmail.com'))
 
 DEFAULT_PAGINATION = 10
 DEFAULT_CATEGORY = 'General'
 DEFAULT_METADATA = {
     'status': 'draft',
 }
-DISPLAY_PAGES_ON_MENU = True
-DISPLAY_CATEGORIES_ON_MENU = True
+
+# Display pages list on the top menu
+DISPLAY_PAGES_ON_MENU = False
+# Display categories list on the top menu
+DISPLAY_CATEGORIES_ON_MENU = False
+
+# Display categories list as a submenu of the top menu
+DISPLAY_CATEGORIES_ON_SUBMENU = False
+
+# Display the category in the article's info
+DISPLAY_CATEGORIES_ON_POSTINFO= False
+
+# Display the author in the article's info
+DISPLAY_AUTHOR_ON_POSTINFO= False
+
+# Display the search form
+DISPLAY_SEARCH_FORM= False
+
+# Sort pages list by a given attribute
+# PAGES_SORT_ATTRIBUTE = Title
+
+# Display the "Fork me on Github" banner
+GITHUB_URL = None
+
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+
+MENUITEMS = [('Blog', '/'),
+             ('About', 'pages/about.html'),
+             ('CV', 'pages/cv.html'),
+             ('Archives', '/archives.html'),
+             ]
 
 NEWEST_FIRST_ARCHIVES = True
 
@@ -62,6 +90,6 @@ MARKDOWN = {
     'output_format': 'html5',
 }
 
-THEME = 'themes/pelican-alchemy/alchemy'
+THEME = 'themes/pelican-blueidea'
 SITEIMAGE = '/images/CV-2.png width=90 height=90'
 
